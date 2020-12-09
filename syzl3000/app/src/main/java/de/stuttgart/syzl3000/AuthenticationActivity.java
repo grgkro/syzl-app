fd64a51c61ebe36ec62a14dcbe3dc39196437a53
Georg <georgkromer@pm.me>
2020-12-09 08:29:17 +0100

    basic authentication flow works (signup, confirm, login)

--- syzl3000/app/src/main/java/de/stuttgart/syzl3000/AuthenticationActivity.java
+++ syzl3000/app/src/main/java/de/stuttgart/syzl3000/AuthenticationActivity.java
@@ -2,7 +2,6 @@ package de.stuttgart.syzl3000;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,18 +10,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.Callback;
-import com.amazonaws.mobile.client.SignInUIOptions;
-import com.amazonaws.mobile.client.UserStateDetails;
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.auth.AuthUserAttributeKey;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.auth.options.AuthSignUpOptions;
 import com.amplifyframework.core.Amplify;
-import com.google.android.material.textfield.TextInputEditText;
-
-import java.util.concurrent.BlockingDeque;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
@@ -30,6 +22,9 @@ public class AuthenticationActivity extends AppCompatActivity {
 
     private EditText editTextEmail;
     private EditText editTextPassword;
+    private EditText editTextConfirmationCode;
+    private Button signUpBtn;
+    private Button confirmSignUpBtn;
     private Button loginBtn;
 
     @Override
@@ -38,7 +33,24 @@ public class AuthenticationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_authentication);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmationCode = findViewById(R.id.editTextConfirmationCode);
         loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        confirmSignUpBtn = findViewById(R.id.confirmSignUpBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUpBtnClicked();
+            }
+        });
+
+        confirmSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmSignUpBtnClicked();
+            }
+        });
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,68 +59,69 @@ public class AuthenticationActivity extends AppCompatActivity {
             }
         });
 
-//        try {
-//            Amplify.addPlugin(new AWSCognitoAuthPlugin());
-//            Log.d(TAG, "onCreate: plugin added");
-//            Amplify.configure(getApplicationContext());
-//            Log.i("MyAmplifyApp", "Initialized Amplify");
-//            Log.d(TAG, "onCreate: configured");
-//
-//
-////            Amplify.Auth.confirmSignUp(
-////                    "username",
-////                    "the code you received via email",
-////                    result -> Log.i("AuthQuickstart", result.isSignUpComplete() ? "Confirm signUp succeeded" : "Confirm sign up not complete"),
-////                    error -> Log.e("AuthQuickstart", error.toString())
-////            );
-//        } catch (AmplifyException error) {
-//            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
-//        }
-//
-//        signOut();
-//        Amplify.Auth.fetchAuthSession(
-//                result -> Log.i("AmplifyQuickstart", result.toString()),
-//                error -> Log.e("AmplifyQuickstart", error.toString())
-//        );
+        try {
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Log.d(TAG, "onCreate: plugin added");
+            Amplify.configure(getApplicationContext());
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+            Log.d(TAG, "onCreate: configured");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
+
+        signOut();
+        Amplify.Auth.fetchAuthSession(
+                result -> Log.i("AmplifyQuickstart", result.toString()),
+                error -> Log.e("AmplifyQuickstart", error.toString())
+        );
 
 
-        AWSMobileClient.getInstance().signOut();
-        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
 
-            @Override
-            public void onResult(UserStateDetails userStateDetails) {
-                Log.i(TAG, userStateDetails.getUserState().toString());
-                switch (userStateDetails.getUserState()){
-                    case SIGNED_IN:
-                        Log.d(TAG, "onResult: Login successful");
-                        Intent i = new Intent(AuthenticationActivity.this, SelectTopCategoryActivity.class);
-                        AuthenticationActivity.this.startActivity(i);
-                        break;
-                    case SIGNED_OUT:
-                        showSignIn();
-                        break;
-                    default:
-                        AWSMobileClient.getInstance().signOut();
-                        showSignIn();
-                        break;
-                }
-            }
-
-            @Override
-            public void onError(Exception e) {
-                Log.d(TAG, "onError: error");
-                Log.e(TAG, e.toString());
-            }
-        });
     }
 
+    private void signUpBtnClicked() {
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+        Toast.makeText(this, email+password, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "awesomeButtonClicked: " + email + password);
+        signUp(email, password);
+    }
+
+    private void confirmSignUpBtnClicked() {
+        String email = editTextEmail.getText().toString();
+        String confirmationCode = editTextConfirmationCode.getText().toString();
+        Amplify.Auth.confirmSignUp(
+                email,
+                confirmationCode,
+                result -> Log.i("AuthQuickstart", result.isSignUpComplete() ? "Confirm signUp succeeded" : "Confirm sign up not complete"),
+                error -> Log.e("AuthQuickstart", error.toString())
+        );
+    }
 
     private void loginBtnClicked() {
         String email = editTextEmail.getText().toString();
         String password = editTextPassword.getText().toString();
         Toast.makeText(this, email+password, Toast.LENGTH_SHORT).show();
         Log.d(TAG, "awesomeButtonClicked: " + email + password);
-        signUp(email, password);
+        login(email, password);
+    }
+
+    private void login(String email, String password) {
+        Amplify.Auth.signIn(
+                email,
+                password,
+                result -> {
+                    if (result.isSignInComplete()) {
+                        Log.i(TAG, "Sign in succeeded");
+                        Intent i = new Intent(AuthenticationActivity.this, SelectTopCategoryActivity.class);
+                        AuthenticationActivity.this.startActivity(i);
+                    } else {
+                        Log.i(TAG,  "Sign in not complete");
+                    }
+
+                },
+                error -> Log.e(TAG, error.toString())
+        );
     }
 
     private void signUp(String email, String password) {
@@ -119,39 +132,6 @@ public class AuthenticationActivity extends AppCompatActivity {
                 result -> Log.i("AuthQuickStart", "Result: " + result.toString()),
                 error -> Log.e("AuthQuickStart", "Sign up failed", error)
         );
-    }
-
-    private void showSignIn() {
-        try {
-            AWSMobileClient.getInstance().showSignIn(this,
-                    SignInUIOptions.builder().nextActivity(SelectTopCategoryActivity.class).build(),
-                    new Callback<UserStateDetails>() {
-                        @Override
-                        public void onResult(UserStateDetails result) {
-                            Log.d(TAG, "onResult: " + result.getUserState());
-                            switch (result.getUserState()) {
-                                case SIGNED_IN:
-                                    Log.i("INIT", "logged in!");
-                                    break;
-                                case SIGNED_OUT:
-                                    Log.i(TAG, "onResult: User did not choose to sign-in");
-                                    break;
-                                default:
-                                    AWSMobileClient.getInstance().signOut();
-                                    break;
-                            }
-                        }
-
-                        @Override
-                        public void onError(Exception e) {
-                            Log.e(TAG, "onError: ", e);
-                        }
-                    }
-                    );
-        } catch (Exception e) {
-            Log.d(TAG, "showSignIn: failed");
-            Log.e(TAG, e.toString());
-        }
     }
 
     private void signOut() {
