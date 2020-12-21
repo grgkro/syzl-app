6552a58cdb92eaa400554fbcae804a53ec586fc8
Georg <georgkromer@pm.me>
2020-12-21 23:45:25 +0100

    fixed too big image bug with drawable-xxhdpi

--- syzl3000/app/src/main/java/de/stuttgart/syzl3000/menu/ProfileActivity.java
+++ syzl3000/app/src/main/java/de/stuttgart/syzl3000/menu/ProfileActivity.java
@@ -25,31 +25,23 @@ import de.stuttgart.syzl3000.CircleListActivity;
 import de.stuttgart.syzl3000.LambdaSimpleProxyClient;
 import de.stuttgart.syzl3000.R;
 import de.stuttgart.syzl3000.RecipeListActivity;
+import de.stuttgart.syzl3000.SelectTopCategoryActivity;
+import de.stuttgart.syzl3000.authentication.SignUpActivity;
 import de.stuttgart.syzl3000.services.AuthService;;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private static final String TAG = "ProfileActivity";
 
-    private Button deleteAccountBtn;
-    private AuthService authService;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        deleteAccountBtn = findViewById(R.id.deleteAccountBtn);
-        authService = new AuthService();
-
-
     }
 
     public void deleteAccount(View view) {
         Log.d(TAG, "deleteAccount: delete Btn clicked");
         new MyTask().execute();
-
-        Toast.makeText(this, "going to delete Account", Toast.LENGTH_SHORT).show();
     }
 
     private class MyTask extends AsyncTask<Void, Void, Void> {
@@ -59,14 +51,22 @@ public class ProfileActivity extends AppCompatActivity {
             try {
                 ApiClientFactory factory = new ApiClientFactory();
                 final LambdaSimpleProxyClient client = factory.build(LambdaSimpleProxyClient.class);
-                client.usersDelete("c71d5514-da33-42c2-9f95-e7a41bf9e7df");
+                client.usersDelete("0d59eebf-b94c-429d-b351-942e7807f1e1");
                 result = "Success";
+                Intent intent = new Intent(ProfileActivity.this, SignUpActivity.class);
+                startActivity(intent);
                 return null;
             } catch (Exception e) {
                 Log.e(TAG, "doInBackground: ", e);
                 result = e.getMessage();
             }
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
         }
     }
 
