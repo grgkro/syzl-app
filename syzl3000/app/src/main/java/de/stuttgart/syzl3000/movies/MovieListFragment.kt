e2e83963267190defd1b84f35fde48e8efb993ba
Georg <georgkromer@pm.me>
2021-01-02 12:44:50 +0100

    added 2nd fragment and navigation

--- syzl3000/app/src/main/java/de/stuttgart/syzl3000/movies/MovieListFragment.kt
+++ syzl3000/app/src/main/java/de/stuttgart/syzl3000/movies/MovieListFragment.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
@@ -20,9 +21,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.AmbientContext
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 import de.stuttgart.syzl3000.R
 
 class MovieListFragment : Fragment() {
@@ -32,23 +37,26 @@ class MovieListFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout
-                .fragment_movie_list, container, false)
-        view.findViewById<ComposeView>(R.id.compose_view).setContent {
-            Column(modifier = Modifier
-                    .border(border = BorderStroke(1.dp, Color.Black))
-                    .padding(16.dp)
-            ) {
-                Text("THIS IS A COMPOSABLE INSIDE THE FRAGMENT")
-                Spacer(modifier = Modifier.padding(10.dp))
-                CircularProgressIndicator()
-                Spacer(modifier = Modifier.padding(10.dp))
-                Text("NEAT")
-                Spacer(modifier = Modifier.padding(10.dp))
-                val customView = HorizontalDottedProgress(AmbientContext.current)
-                AndroidView(viewBlock = {customView})
+
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                            text ="Movie LIst",
+                            style = TextStyle(
+                                    fontSize = TextUnit.Companion.Sp(21)
+                            )
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(
+                            onClick = {
+                                findNavController().navigate(R.id.viewMovie)
+                            }
+                    ) {
+                        Text(text = "To movie Fragment")
+                    }
+                }
             }
         }
-        return view
     }
 }
